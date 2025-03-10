470c43a895b5fe8e104e9ac6a7fc333497bdeb41
672654 <henrik_krane@hotmail.com>
2025-03-10 14:53:38 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using SpaceSim;
+
+
+class Astronomy
+{
+
+    public static void Main()
+    {
+        List<SpaceObject> solarSystem = new List<SpaceObject>();
+
+        solarSystem.Add(new Star("Sun", 0, 0, 100, 100, "Yellow"));
+        solarSystem.Add(new Planet("Mars", 300, 686, 100, 200, "orange"));
+        solarSystem.Add(new Planet("Jupiter", 350, 4332, 100, 200, "grey"));
+        solarSystem.Add(new Planet("Earth", 400, 365, 100, 200, "blue"));
+        solarSystem.Add(new Moon("The Mooen", 450, 200, 100, 200, "white", solarSystem.Find(p => p.Name == "Earth")));
+        solarSystem.Add(new Comet("C01", 100, 2000, 100, 2000, "black"));
+        solarSystem.Add(new Asteroid("A01", 200, 1000, 100, 1000, "black"));        
+        
+        while (true)
+        {
+            Console.Write("Enter time (number of days since time = 0: ");
+            int time = Convert.ToInt32(Console.ReadLine());
+           
+
+            Console.Write("Enter name of Planet or write Sun for the sun: ");
+            string spaceObjectString = Console.ReadLine().Trim();
+
+            spaceObjectString = string.IsNullOrWhiteSpace(spaceObjectString) ? "Sun" : spaceObjectString;
+
+            SpaceObject spaceObject = solarSystem.Find(s  => s.Name == spaceObjectString);
+
+            if (solarSystem.Contains(spaceObject))
+            {
+                spaceObject.displayInfo(time);
+            }
+
+        }
+
+
+    }
+}
---
+++ oblig2/SpaceObject.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SpaceSim {
+
+    public class SpaceObject
+    {
+        public string Name { get; protected set; }
+
+        public int OrbitalRadius { get; protected set; }
+        public int OrbitalPeriod {  get; protected set; }
+        public int ObjectRadius { get; protected set; }
+        public int RotationalPeriod {  get; protected set; }
+        public string ObjectColor { get; protected set; }
+
+
+        
+
+
+        public SpaceObject(string name, int orbitalRadius, int orbitalPeriod, int objectRadius, int rotationalPerioid, string color)
+        {
+            Name = name;
+            OrbitalRadius = orbitalRadius;
+            OrbitalPeriod = orbitalPeriod;
+            ObjectRadius = objectRadius;
+            RotationalPeriod = rotationalPerioid;
+            ObjectColor = color;
+        }
+
+        public virtual void Draw()
+        {
+            Console.WriteLine(Name);
+        }
+
+        public void SetOrbitalRadius(int r)
+        {
+            OrbitalRadius = r;
+        }
+        public int GetOrbitalRadius()
+        {
+            return OrbitalRadius;
+        }
+        public void SetOrbitalPeriod(int period)
+        {
+            OrbitalPeriod = period;
+        }
+        public int GetOrbitalPeriod()
+        {
+            return OrbitalPeriod;
+        }
+        public void SetObjectRadius(int radius)
+        {
+            ObjectRadius = radius;
+        }
+        public int GetObjectRadius()
+        {
+            return ObjectRadius;
+        }
+        public void SetObjectColor(string color)
+        {
+            ObjectColor = color;
+        }
+        public string GetObjectColor()
+        {
+            return ObjectColor;
+        }
+
+        public (int x, int y) CalculatePositions(int time)
+        {
+
+            double angle = 360 * (time / OrbitalPeriod); // Calculate angle in degrees
+            double radians = angle * (Math.PI/180); // Convert to radians
+
+            int x = (int)(GetOrbitalRadius() * Math.Cos(radians));
+            int y = (int)(GetOrbitalRadius() * Math.Sin(radians));
+
+            return (x, y);
+        }
+
+        public void displayInfo(int time)
+        {
+            Console.WriteLine(this.Name);
+        }
+                
+    }
+
+    public class Star : SpaceObject
+    {
+        public Star(string name, int orbitalRadius, int orbitalPeriod, int objectRadius, int rotationalPerioid, string color) : base(name, orbitalRadius, orbitalPeriod, objectRadius, rotationalPerioid, color) {
+            
+        }
+        public override void Draw()
+        {
+            Console.Write("Star : " );
+            base.Draw();
+        }
+    }
+
+    public class Planet : SpaceObject
+    {
+        
+        public Planet(string name, int orbitalRadius, int orbitalPeriod, int objectRadius, int rotationalPerioid, string color) : base(name, orbitalRadius, orbitalPeriod, objectRadius, rotationalPerioid, color) {
+           
+        
+        }
+        public override void Draw() {
+            Console.Write("Planet : ");
+            base.Draw();
+        }
+           
+    }
+
+    public class Moon : SpaceObject
+    {
+        public SpaceObject ParentPlanet {  get; private set; }
+        public Moon(string name, int orbitalRadius, int orbitalPeriod, int objectRadius, int rotationalPerioid, string color, SpaceObject parent) 
+            : base(name, orbitalRadius, orbitalPeriod, objectRadius, rotationalPerioid, color) 
+        { 
+            ParentPlanet = parent;
+        }
+        public override void Draw()
+        {
+            Console.Write("Moon : ");
+            base.Draw();
+        }
+    }
+
+    public class Comet : SpaceObject
+    {
+        public Comet(string name, int orbitalRadius, int orbitalPeriod, int objectRadius, int rotationalPerioid, string color) : base(name, orbitalRadius, orbitalPeriod, objectRadius, rotationalPerioid, color) { }
+
+        public override void Draw()
+        {
+            Console.Write("Comet : ");
+            base.Draw();
+        }
+    }
+
+
+    public class Asteroid : SpaceObject
+    {
+        public Asteroid(string name, int orbitalRadius, int orbitalPeriod, int objectRadius, int rotationalPerioid, string color) : base(name, orbitalRadius, orbitalPeriod, objectRadius, rotationalPerioid, color) { }
+
+        public override void Draw()
+        {
+            Console.Write("Asteroid : ");
+            base.Draw();
+        }
+    }
+
+}
